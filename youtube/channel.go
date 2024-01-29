528f0213e9db7156c14685d3700187b8cdadfbdf
apple <apple.mangoooo97@gmail.com>
2024-01-29 22:53:36 +0900

    add cache and more

--- youtube/channel.go
+++ youtube/channel.go
@@ -1,13 +1,29 @@
 package youtube
 
-import "google.golang.org/api/youtube/v3"
+import (
+	"google.golang.org/api/youtube/v3"
+	"vdule/cache"
+)
+
+func GetChannelByHandleCache(handle string) (*youtube.Channel, bool) {
+	cacheId := cache.GetCacheId("channel", "handle", handle)
+	if c, in := cache.GetCache(cacheId, &youtube.Channel{}); in {
+		return c, true
+	}
+	return nil, false
+}
 
 func (t *Tube) GetChannelByHandle(handle string) (*youtube.Channel, error) {
-	call := t.Service.Channels.List([]string{"snippet", "contentDetails"}).ForHandle(handle)
+	if c, in := GetChannelByHandleCache(handle); in {
+		return c, nil
+	}
+	call := t.Service.Channels.List([]string{"snippet", "contentDetails", "brandingSettings"}).ForHandle(handle)
 	res, err := call.Do()
 	if err != nil {
 		return nil, err
 	}
+	cacheId := cache.GetCacheId("channel", "handle", handle)
+	_ = cache.PushCache(cacheId, res.Items[0])
 	return res.Items[0], nil
 }
 
--- youtube/video.go
+++ youtube/video.go
@@ -3,45 +3,10 @@ package youtube
 import (
 	"errors"
 	"google.golang.org/api/youtube/v3"
+	"strconv"
+	"vdule/cache"
+	"vdule/utils"
 )
-
-func (t *Tube) GetPlayListVideosId(id string, max int64) ([]string, error) {
-	if max > 50 {
-		return nil, errors.New("error")
-	}
-	call := t.Service.PlaylistItems.List([]string{"contentDetails"}).PlaylistId(id).MaxResults(max)
-	res, err := call.Do()
-	if err != nil {
-		return nil, err
-	}
-	var videos []string
-	for _, item := range res.Items {
-		videos = append(videos, item.ContentDetails.VideoId)
-	}
-	return videos, nil
-}
-
-func (t *Tube) VideoIdsToVideos(ids []string) ([]*TubeVideo, error) {
-	var videos []*TubeVideo
-	for _, id := range ids {
-		video, err := t.GetVideo(id)
-		if err != nil {
-			return nil, err
-		}
-		videos = append(videos, video)
-	}
-	return videos, nil
-}
-
-func FilterLive(videos []*TubeVideo) []*TubeVideo {
-	var res []*TubeVideo
-	for _, video := range videos {
-		if video.isLive {
-			res = append(res, video)
-		}
-	}
-	return res
-}
 
 type TubeVideo struct {
 	Id                     string
@@ -64,6 +29,63 @@ type TubeVideo struct {
 	Duration string
 }
 
+func GetPlayListVideosIdCache(id string, max int64) ([]string, bool) {
+	cacheId := cache.GetCacheId("playlist", id, strconv.FormatInt(max, 10))
+	if c, in := cache.GetCache(cacheId, &youtube.PlaylistItemListResponse{}); in {
+		var videos []string
+		for _, item := range c.Items {
+			videos = append(videos, item.ContentDetails.VideoId)
+		}
+		return videos, true
+	}
+	return []string{}, false
+}
+
+func (t *Tube) GetPlayListVideosId(id string, max int64) ([]string, error) {
+	if c, in := GetPlayListVideosIdCache(id, max); in {
+		return c, nil
+	}
+	if max > 50 {
+		return nil, errors.New("error")
+	}
+	call := t.Service.PlaylistItems.List([]string{"contentDetails"}).PlaylistId(id).MaxResults(max)
+	res, err := call.Do()
+	if err != nil {
+		return nil, err
+	}
+	cacheId := cache.GetCacheId("playlist", id, strconv.FormatInt(max, 10))
+	_ = cache.PushCache(cacheId, res)
+	var videos []string
+	for _, item := range res.Items {
+		videos = append(videos, item.ContentDetails.VideoId)
+	}
+	return videos, nil
+}
+
+func (t *Tube) VideoIdsToVideos(ids []string) ([]*TubeVideo, error) {
+	var videos []*TubeVideo
+	for _, id := range ids {
+		video, err := t.GetVideo(id)
+		if err != nil {
+			return nil, err
+		}
+		videos = append(videos, video)
+	}
+	return videos, nil
+}
+
+func FilterLive(videos []*TubeVideo) []*TubeVideo {
+	return utils.Filter(videos, func(video *TubeVideo) bool {
+		return video.isLive
+	})
+}
+
+func FilterUpcoming(videos []*TubeVideo) []*TubeVideo {
+	return utils.Filter(videos, func(video *TubeVideo) bool {
+		return video.isLiveUpcoming
+	})
+}
+
 func isLive(video *youtube.Video) bool {
 	if video.LiveStreamingDetails != nil {
 		return true
@@ -71,12 +93,25 @@ func isLive(video *youtube.Video) bool {
 	return false
 }
 
+func GetRawVideoCache(id string) (*youtube.Video, bool) {
+	cacheId := cache.GetCacheId("video", id)
+	if c, in := cache.GetCache(cacheId, &youtube.Video{}); in {
+		return c, true
+	}
+	return nil, false
+}
+
 func (t *Tube) GetRawVideo(id string) (*youtube.Video, error) {
+	if c, in := GetRawVideoCache(id); in {
+		return c, nil
+	}
 	call := t.Service.Videos.List([]string{"snippet", "liveStreamingDetails", "statistics", "contentDetails"}).Id(id)
 	res, err := call.Do()
 	if err != nil {
 		return nil, err
 	}
+	cacheId := cache.GetCacheId("video", id)
+	_ = cache.PushCache(cacheId, res.Items[0])
 	return res.Items[0], nil
 }
 
