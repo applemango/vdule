83391f32fe3ec820bcdad40cd4d2416da6e22374
apple <apple.mangoooo97@gmail.com>
2024-01-29 09:28:17 +0900

    add GetChannelLive and more

--- youtube/channel.go
+++ youtube/channel.go
@@ -2,8 +2,8 @@ package youtube
 
 import "google.golang.org/api/youtube/v3"
 
-func (t Tube) GetChannelByHandle(handle string, parts []string) (*youtube.Channel, error) {
-	call := t.Service.Channels.List(parts).ForHandle(handle)
+func (t *Tube) GetChannelByHandle(handle string) (*youtube.Channel, error) {
+	call := t.Service.Channels.List([]string{"snippet", "contentDetails"}).ForHandle(handle)
 	res, err := call.Do()
 	if err != nil {
 		return nil, err
@@ -11,6 +11,24 @@ func (t Tube) GetChannelByHandle(handle string, parts []string) (*youtube.Channe
 	return res.Items[0], nil
 }
 
-func GetChannelUploadPlayList(channel youtube.Channel) string {
+func GetChannelUploadPlayList(channel *youtube.Channel) string {
 	return channel.ContentDetails.RelatedPlaylists.Uploads
+}
+
+func (t *Tube) GetChannelLive(handle string) ([]*TubeVideo, error) {
+	channel, err := t.GetChannelByHandle(handle)
+	if err != nil {
+		return nil, err
+	}
+	playList := GetChannelUploadPlayList(channel)
+	videosId, err := t.GetPlayListVideosId(playList, 5)
+	if err != nil {
+		return nil, err
+	}
+	videos, err := t.VideoIdsToVideos(videosId)
+	if err != nil {
+		return nil, err
+	}
+	lives := FilterLive(videos)
+	return lives, nil
 }
--- youtube/youtube.go
+++ youtube/youtube.go
@@ -11,13 +11,14 @@ type Tube struct {
 	Service *youtube.Service
 }
 
-func CreateTube() Tube {
+func CreateTube() (*Tube, error) {
 	client := &http.Client{
 		Transport: &transport.APIKey{Key: "AIzaSyA2KkxjSd-s5ydoVPCym9yOH9lsyInxoKE"},
 	}
 	service, err := youtube.New(client)
 	if err != nil {
+		return nil, err
 		log.Fatalf("Error creating new YouTube client: %v", err)
 	}
-	return Tube{service}
+	return &Tube{service}, nil
 }
