d7bf56ac6138f9ff040550ab3f9881ec7e99ecfc
apple <apple.mangoooo97@gmail.com>
2024-02-04 23:25:59 +0900

    add TubeChannel and more

--- youtube/channel.go
+++ youtube/channel.go
@@ -5,7 +5,22 @@ import (
 	"vdule/cache"
 )
 
-func GetChannelByHandleCache(handle string) (*youtube.Channel, bool) {
+type TubeChannel struct {
+	Id              string `json:"id"`
+	Name            string `json:"name"`
+	Description     string `json:"description"`
+	Handle          string `json:"handle"`
+	PublishAt       string `json:"publish_at"`
+	IconImage       string `json:"icon_image"`
+	UploadsPlaylist string `json:"uploads_playlist"`
+	ViewCount       uint64 `json:"view_count"`
+	SubscriberCount uint64 `json:"subscriber_count"`
+	VideoCount      uint64 `json:"video_count"`
+	Trailer         string `json:"trailer"`
+	BannerImage     string `json:"banner_image"`
+}
+
+func GetRawChannelByHandleCache(handle string) (*youtube.Channel, bool) {
 	cacheId := cache.GetCacheId("channel", "handle", handle)
 	if c, in := cache.GetCache(cacheId, &youtube.Channel{}); in {
 		return c, true
@@ -13,11 +28,11 @@ func GetChannelByHandleCache(handle string) (*youtube.Channel, bool) {
 	return nil, false
 }
 
-func (t *Tube) GetChannelByHandle(handle string) (*youtube.Channel, error) {
-	if c, in := GetChannelByHandleCache(handle); in {
+func (t *Tube) GetRawChannelByHandle(handle string) (*youtube.Channel, error) {
+	if c, in := GetRawChannelByHandleCache(handle); in {
 		return c, nil
 	}
-	call := t.Service.Channels.List([]string{"snippet", "contentDetails", "brandingSettings"}).ForHandle(handle)
+	call := t.Service.Channels.List([]string{"snippet", "contentDetails", "brandingSettings", "statistics"}).ForHandle(handle)
 	res, err := call.Do()
 	if err != nil {
 		return nil, err
@@ -27,12 +42,38 @@ func (t *Tube) GetChannelByHandle(handle string) (*youtube.Channel, error) {
 	return res.Items[0], nil
 }
 
+func ChannelToTubeChannel(channel *youtube.Channel) TubeChannel {
+	return TubeChannel{
+		Id:              channel.Id,
+		Name:            channel.Snippet.Title,
+		Description:     channel.Snippet.Description,
+		Handle:          channel.Snippet.CustomUrl[1:],
+		PublishAt:       channel.Snippet.PublishedAt,
+		IconImage:       channel.Snippet.Thumbnails.High.Url,
+		UploadsPlaylist: channel.ContentDetails.RelatedPlaylists.Uploads,
+		ViewCount:       channel.Statistics.ViewCount,
+		SubscriberCount: channel.Statistics.SubscriberCount,
+		VideoCount:      channel.Statistics.VideoCount,
+		//Trailer:         channel.BrandingSettings.Channel.UnsubscribedTrailer,		//Trailer:         channel.BrandingSettings.Channel.UnsubscribedTrailer,
+		BannerImage: channel.BrandingSettings.Image.BannerExternalUrl,
+	}
+}
+
+func (t *Tube) GetChannelByHandle(handle string) (*TubeChannel, error) {
+	channel, err := t.GetRawChannelByHandle(handle)
+	if err != nil {
+		return nil, err
+	}
+	c := ChannelToTubeChannel(channel)
+	return &c, nil
+}
+
 func GetChannelUploadPlayList(channel *youtube.Channel) string {
 	return channel.ContentDetails.RelatedPlaylists.Uploads
 }
 
 func (t *Tube) GetChannelLive(handle string) ([]*TubeVideo, error) {
-	channel, err := t.GetChannelByHandle(handle)
+	channel, err := t.GetRawChannelByHandle(handle)
 	if err != nil {
 		return nil, err
 	}
