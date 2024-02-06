dab05ca0b268e2ccbd727d3e57e98080800fa7d2
apple <apple.mangoooo97@gmail.com>
2024-02-06 21:49:37 +0900

    add GetSchedules

---
+++ vtuber/schedule/schedule.go
@@ -0,0 +1,72 @@
+package schedule
+
+import (
+	"database/sql"
+	db "vdule/utils/db/sqlite3"
+	"vdule/vtuber/youtube"
+)
+
+type ScheduleDate struct {
+	Year   int `json:"year,omitempty"`
+	Month  int `json:"month,omitempty"`
+	Day    int `json:"day,omitempty"`
+	Hour   int `json:"hour,omitempty"`
+	Minute int `json:"minute,omitempty"`
+}
+
+type Schedule struct {
+	Id         string              `json:"id,omitempty"`
+	Date       ScheduleDate        `json:"date"`
+	Channel    youtube.TubeChannel `json:"channel"`
+	IsNowOnAir bool                `json:"is_now_on_air,omitempty"`
+	Title      string              `json:"title,omitempty"`
+	Thumbnail  string              `json:"thumbnail,omitempty"`
+}
+
+type Row interface {
+	*sql.Row | *sql.Rows
+	Scan(dest ...any) error
+}
+
+func GetScheduleCore[T Row](row T) (*Schedule, error) {
+	var (
+		s Schedule
+		d ScheduleDate
+	)
+	var handle string
+	err := row.Scan(&s.Id, &handle, &s.Title, &s.Thumbnail, &s.IsNowOnAir, &d.Year, &d.Month, &d.Day, &d.Hour, &d.Minute)
+	if err != nil {
+		return nil, err
+	}
+	channel, in := youtube.GetRawChannelByHandleCache(handle)
+	if !in {
+		return nil, err
+	}
+	s.Channel = youtube.ChannelToTubeChannel(channel)
+	s.Date = d
+	return &s, nil
+}
+
+func GetSchedulesCore(query string, args ...any) ([]Schedule, error) {
+	var schedules []Schedule
+	rows, err := db.Conn.Query(query, args...)
+	if err != nil {
+		return schedules, err
+	}
+	for rows.Next() {
+		schedule, err := GetScheduleCore(rows)
+		if err != nil {
+			continue
+		}
+		schedules = append(schedules, *schedule)
+	}
+	return schedules, nil
+}
+
+func GetSchedules(year, month, day int) ([]Schedule, error) {
+	return GetSchedulesCore(`SELECT id, handle, title, thumbnail, is_now_on_air, live_scheduled_start_year, live_scheduled_start_month, live_scheduled_start_day, live_scheduled_start_hour, live_scheduled_start_minute FROM video WHERE live_scheduled_start_year = ? AND live_scheduled_start_month = ? AND live_scheduled_start_day = ? AND is_live = true`, year, month, day)
+}
+
+func GetChannelSchedules(year, month, day int, handle string) ([]Schedule, error) {
+	return GetSchedulesCore(`SELECT id, handle, title, thumbnail, is_now_on_air, live_scheduled_start_year, live_scheduled_start_month, live_scheduled_start_day, live_scheduled_start_hour, live_scheduled_start_minute FROM video WHERE live_scheduled_start_year = ? AND live_scheduled_start_month = ? AND live_scheduled_start_day = ? AND is_live = true AND handle = ?`, year, month, day, handle)
+}
