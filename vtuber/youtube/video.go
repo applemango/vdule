c454dde95c8d90caf911c4293529bb5718ec539f
apple <apple.mangoooo97@gmail.com>
2024-02-08 10:52:24 +0900

    add LiveDate and more

--- vtuber/youtube/video.go
+++ vtuber/youtube/video.go
@@ -10,23 +10,26 @@ import (
 )
 
 type TubeVideo struct {
-	Id                     string   `json:"id,omitempty"`
-	ChannelId              string   `json:"channel_id,omitempty"`
-	Title                  string   `json:"title,omitempty"`
-	Description            string   `json:"description,omitempty"`
-	ViewCount              uint64   `json:"view_count,omitempty"`
-	LikeCount              uint64   `json:"like_count,omitempty"`
-	CommentCount           uint64   `json:"comment_count,omitempty"`
-	PublishedAt            string   `json:"published_at,omitempty"`
-	Tags                   []string `json:"tags,omitempty"`
-	LiveBroadCastContent   string   `json:"live_broad_cast_content,omitempty"`
-	IsLive                 bool     `json:"is_live,omitempty"`
-	IsLiveUpcoming         bool     `json:"is_live_upcoming,omitempty"`
-	LiveActualStartTime    string   `json:"live_actual_start_time,omitempty"`
-	LiveActualEndTime      string   `json:"live_actual_end_time,omitempty"`
-	LiveScheduledStartTime string   `json:"live_scheduled_start_time,omitempty"`
-	LiveScheduledEndTime   string   `json:"live_scheduled_end_time,omitempty"`
-	Duration               string   `json:"duration,omitempty"`
+	Id                     string    `json:"id,omitempty"`
+	ChannelId              string    `json:"channel_id,omitempty"`
+	Title                  string    `json:"title,omitempty"`
+	Description            string    `json:"description,omitempty"`
+	ViewCount              uint64    `json:"view_count,omitempty"`
+	LikeCount              uint64    `json:"like_count,omitempty"`
+	CommentCount           uint64    `json:"comment_count,omitempty"`
+	PublishedAt            string    `json:"published_at,omitempty"`
+	Tags                   []string  `json:"tags,omitempty"`
+	LiveBroadCastContent   string    `json:"live_broad_cast_content,omitempty"`
+	IsLive                 bool      `json:"is_live,omitempty"`
+	IsLiveUpcoming         bool      `json:"is_live_upcoming,omitempty"`
+	IsLiveNowOnAir         bool      `json:"is_live_now_on_air,omitempty"`
+	LiveActualStartTime    string    `json:"live_actual_start_time,omitempty"`
+	LiveActualEndTime      string    `json:"live_actual_end_time,omitempty"`
+	LiveScheduledStartTime string    `json:"live_scheduled_start_time,omitempty"`
+	LiveScheduledEndTime   string    `json:"live_scheduled_end_time,omitempty"`
+	LiveDate               time.Time `json:"live_date,omitempty"`
+	Duration               string    `json:"duration,omitempty"`
+	Thumbnail              string    `json:"thumbnail,omitempty"`
 }
 
 func GetPlayListVideosIdCache(id string, max int64) ([]string, bool) {
@@ -115,13 +118,32 @@ func (t *Tube) GetRawVideo(id string) (*youtube.Video, error) {
 	return res.Items[0], nil
 }
 
+func ParseYoutubeApiDate(datetime string) time.Time {
+	/*
+	 * youtube data api v3の時刻はRFC3339でタイムゾーンはUTCなので
+	 * 扱いやすいようにtime.Timeのjstに変換する
+	 */
+	t, err := time.Parse("2006-01-02T15:04:05Z", datetime)
+	if err != nil {
+		panic(err.Error())
+	}
+	t = t.Add(time.Hour * 9)
+	return t
+}
+
 func VideoToTubeVideo(item *youtube.Video) TubeVideo {
-	actualStartTime, actualEndTime, scheduledStartTime, scheduledEndTime := "", "", "", ""
+	actualStartTime, actualEndTime, scheduledStartTime, scheduledEndTime, t := "", "", "", "", time.Time{}
 	if item.LiveStreamingDetails != nil {
 		actualStartTime = item.LiveStreamingDetails.ActualStartTime
 		actualEndTime = item.LiveStreamingDetails.ActualEndTime
 		scheduledStartTime = item.LiveStreamingDetails.ScheduledStartTime
 		scheduledEndTime = item.LiveStreamingDetails.ScheduledEndTime
+		if len(scheduledStartTime) != 0 {
+			t = ParseYoutubeApiDate(scheduledStartTime)
+		}
+		if len(actualStartTime) != 0 {
+			t = ParseYoutubeApiDate(actualStartTime)
+		}
 	}
 	return TubeVideo{
 		Id:                     item.Id,
@@ -136,11 +158,14 @@ func VideoToTubeVideo(item *youtube.Video) TubeVideo {
 		LiveBroadCastContent:   item.Snippet.LiveBroadcastContent,
 		IsLive:                 isLive(item),
 		IsLiveUpcoming:         item.Snippet.LiveBroadcastContent == "upcoming",
+		IsLiveNowOnAir:         item.Snippet.LiveBroadcastContent == "live",
 		LiveActualStartTime:    actualStartTime,
 		LiveActualEndTime:      actualEndTime,
 		LiveScheduledStartTime: scheduledStartTime,
 		LiveScheduledEndTime:   scheduledEndTime,
+		LiveDate:               t,
 		Duration:               item.ContentDetails.Duration,
+		Thumbnail:              item.Snippet.Thumbnails.High.Url,
 	}
 }
 
